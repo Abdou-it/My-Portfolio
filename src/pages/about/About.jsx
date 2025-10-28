ab8af304354494ae59a106f11c37c651c80d2472
abdou <abdelbassetblil@gmail.com>
2025-10-28 17:48:06 +0100

    Completed the [About Resume & Whole About] section

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,7 +4,9 @@ import Stats from '../../Components/Stats'
 import { FaDownload } from 'react-icons/fa'
 import CV from "../../assets/steve-Cv.pdf"
 import "./about.css"
+import { resume } from '../../data'
 import Skills from '../../Components/Skills'
+import ResumeItem from '../../Components/ResumeItem'
 const About = () => {
     return (
         <main className="section container">
@@ -17,7 +19,7 @@ const About = () => {
                         <ul className="info__list grid">
                             <Info />
                         </ul>
-                        <a href={CV} download='' className="button">Dowload CV
+                        <a href={CV} download='' className="button">Dowload Cv
                             <span className='button__icon'>
                                 <FaDownload />
                             </span>
@@ -41,6 +43,30 @@ const About = () => {
             </section>
 
             <div className="separator"></div>
+
+            <section className="resume">
+                <h3 className='section__subtitle subtitle__center'>
+                    Experience & Education
+                </h3>
+
+                <div className="resume__container grid">
+                    <div className="resume__data">
+                        {resume.map((val)=> {
+                            if (val.category === 'experience') {
+                                return <ResumeItem key={val.id} {...val}/>
+                            }
+                        })}
+                    </div>
+
+                    <div className="resume__data">
+                        {resume.map((val)=> {
+                            if (val.category === 'education') {
+                                return <ResumeItem key={val.id} {...val}/>
+                            }
+                        })}
+                    </div>
+                </div>
+            </section>
 
         </main>
     )
